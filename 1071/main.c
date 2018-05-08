9d0dac515af617ea3a26924d8f20b413fdd8914e
Mateus Furquim <furquim.axs@gmail.com>
2018-05-08 10:59:52 -0300

    [C] Solução para Soma de Impares Consecutivos I

--- 1071/main.c
+++ 1071/main.c
@@ -8,7 +8,23 @@ int main()
 	int n1, n2;
 	scanf("%d %d", &n1, &n2);
 
-	printf("%d e %d\n", n1, n2);
+	int max_value = max(n1,n2);
+	int min_value = min(n1,n2);
+
+	// Min should start odd
+	if (min_value % 2 == 0) {
+		min_value++;
+	}
+	// If odd, doesn't include itself
+	else {
+		min_value+=2;
+	}
+
+	int sum = 0;
+	// Interval b/w and not including (min,max), only odds
+	for (int i = min_value; i < max_value; sum += i, i+=2);
+
+	printf("%d\n", sum);
 
 	return 0;
 }
